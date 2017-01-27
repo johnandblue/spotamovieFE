1760b1efe12f154a8fd66af1a5fc218d186c2b63
johnandblue <jc@johnandblue.com>
2017-01-27 16:05:23 +0100

    fixed: index cleaned

--- index.ios.js
+++ index.ios.js
@@ -10,35 +10,11 @@ import AppContainer from './app/containers/AppContainer'
 
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Button
+  StyleSheet
 } from 'react-native';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(api)));
-
-store.subscribe(() => {
-  saveState({
-    // events: store.getState().events,
-    // user: store.getState().user
-  });
-});
-
-
-// export default class spotamovieFE extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           SPOT A MOVIE or DIE HARD
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
-
 
 const App = () => (
   <Provider store={store}>
@@ -46,23 +22,5 @@ const App = () => (
   </Provider>
 )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00AA00',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('spotamovieFE', () => App);
