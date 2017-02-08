c6cfe10b30b595e7ef73ee4c31da01ace96fa85d
Rod Reyes <roderick.reyes@gmail.com>
2017-02-08 15:25:34 +0100

    refactor: replace Wizard style classes

--- app/components/Wizard/styles/Wizard.js
+++ app/components/Wizard/styles/Wizard.js
@@ -1,59 +1,55 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  posterCard: {
-    borderRadius: 10,
-    width: 250,
-    height: 400,
-    shadowOffset: {
-      width: 0.5,
-      height: 0.5,
-    },
-    shadowColor: 'black',
-    shadowOpacity: 0.8,
-  },
-  poster: {
-    borderRadius: 10
-  },
-  containerLoader: {
+  container: {
     backgroundColor: '#23222E',
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
+    // flex: 1,
+    // flexDirection: 'column',
+    // alignItems: 'center',
+    // justifyContent: 'center'
   },
-  titleView: {
-    flex: 0.1,
-    // backgroundColor: 'pink',
-    flexDirection: 'column',
-    alignItems:'center',
-    justifyContent: 'center',
-    marginTop: 40,
+  headingContainer: {
+    backgroundColor: 'red'
   },
-  title: {
+  heading: {
     fontFamily: 'Raleway-Bold',
     fontSize: 30,
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
-    // backgroundColor: 'purple',
-    color: 'white',
-    marginBottom:10,
   },
-});
-
-export const buttonStyle = StyleSheet.create({
-  start:{
+  subheadingContainer: {
+    paddingTop: 30,
+    paddingRight: 70,
+    paddingLeft: 70,
+  },
+  subheading: {
+    color: 'white',
+    fontFamily: 'Raleway-Medium',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  skipContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  skip: {
+    color: 'grey',
+    fontFamily: 'Raleway-Medium',
+    fontSize: 12,
+    textAlign: 'center'
+  },
+  start: {
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 10,
-    marginTop: 30,
-    backgroundColor:'#23222E',
+    backgroundColor:'#62C654',
     borderRadius:30,
     borderWidth: 1,
-    borderColor: '#fff'
   },
-  startText:{
-      color:'#fff',
-      textAlign:'center',
-      fontSize: 16
+  startText: {
+    color:'white',
+    textAlign:'center',
+    fontSize: 16
   }
 });
