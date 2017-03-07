4f6b017bbeecada4bdbb7144c66bceb9e8c58c41
Rod Reyes <roderick.reyes@gmail.com>
2017-03-07 19:26:35 +0100

    refactor: update config file with api_key

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
  client_id : 'df38bfb3200c41b7b038828c043f6bfa', // Your client id
  redirect_uri : 'spotamovie://callback', // Your redirect uri
+ api_key: 'YOUR_API_KEY'
 }
--- index.ios.js
+++ index.ios.js
@@ -7,22 +7,22 @@ import { composeWithDevTools } from 'remote-redux-devtools';
 import reducers from  './app/reducers/reducers';
 import api      from './app/lib/api.js';
 import AppContainer from './app/containers/AppContainer'
-import Login from './app/containers/Login'
-import { SpotifySymbol, TMDBSymbol } from './app/actions/actions'
+import Login from './app/containers/Login';
+import { SpotifySymbol, TMDBSymbol } from './app/actions/actions';
+import config from '../../config';
+
 
 import {
   AppRegistry,
   StyleSheet
 } from 'react-native';
 
-// const API_KEY = 'b7e6c4c8913b06fd1a52159e1aa7f343';
-
 const composeEnhancers = composeWithDevTools({ realtime: true });
 const store = createStore(reducers, composeEnhancers(
   applyMiddleware(
     // api(SpotifySymbol,  'https://private-bd796b-spotamovie.apiary-mock.com'),
     api(SpotifySymbol,  'https://spotamovie.herokuapp.com'),
-    api(TMDBSymbol,     'https://api.themoviedb.org/3', '?api_key=b7e6c4c8913b06fd1a52159e1aa7f343')
+    api(TMDBSymbol,     'https://api.themoviedb.org/3', '?api_key='+config.api_key)
   )
 ));
 
